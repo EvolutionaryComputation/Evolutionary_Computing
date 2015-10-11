791cb0071e087a88e8318250891d51828c163cbc
Nana <Nana@10.0.0.10>
2015-10-11 11:12:33 -0500

    Made small changes to take input of user data.

--- src/Evolutionary_Computing/EC_Main.java
+++ src/Evolutionary_Computing/EC_Main.java
@@ -1,10 +1,16 @@
 package Evolutionary_Computing;
 
+import java.util.Scanner;
+import java.lang.*;
+import java.util.*;
 
 public class EC_Main {
 
 	public static void main(String[] args) {
-		BinaryTree tree = new BinaryTree();
+/*Add input data or random number generated into tree created by Brian.
+ * Will take testers input data, generate a list of random data and  place into binary tree constructor.		
+ */
+        BinaryTree tree = new BinaryTree();
 		tree.insert(new Node(10));
 		tree.insert(new Node('+'));
 		tree.insert(new Node(12));
@@ -15,6 +21,20 @@ public class EC_Main {
 		tree.printTree();
 		System.out.println("Depth: " + tree.Depth());
 		System.out.println("Size: " + tree.Size());
+		
+		Scanner input = new Scanner(System.in);
+		int data;
+		int data2;
+		
+		System.out.print("Enter first training data: ");
+		data = input.nextInt();
+		
+		System.out.print("Enter second training data range: ");
+		data2 = input.nextInt();
+		
+		
+		
+		
 	}
 
 }
